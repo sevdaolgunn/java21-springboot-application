dfc88267873a7580aaec4d7fae99cc70e1b03f27
sevdaolgunn <sevdaolgun111@hotmail.com>
2024-07-23 16:37:15 +0300

    inital commit

---
+++ src/main/java/com/koza/etiyaspringbootapplication/EtiyaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.koza.etiyaspringbootapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtiyaSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtiyaSpringbootApplication.class, args);
+    }
+
+}
